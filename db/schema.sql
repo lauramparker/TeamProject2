bc77b58a20bcf3677555211f57d99a62bc2d4c61
Chimera <j.calderon3@gmail.com>
2021-01-30 13:40:28 -0500

    schema and seed data updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,21 @@
-//
+-- Identify database
 
 CREATE DATABASE bucket_db;
 USE bucket_db;
 
-CREATE TABLE user ();
+CREATE TABLE user
+(
+    id INT NOT NULL,
+    name VARCHAR (255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travel (
+    id INT NOT NULL,
+    city_name VARCHAR (255) NOT NULL,
+    city_review DECIMAL (10,4) NOT NULL,
+    hotel_name VARCHAR (255) NOT NULL,
+    hotel_review DECIMAL (10,4) NOT NULL,
+    PRIMARY KEY (id),
+    -- FOREIGN KEY??
+);
