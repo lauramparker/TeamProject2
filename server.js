de1cb71edbca2d473e22b43e5105b4969c2f6787
Joe Maniaci <joe.maniaci1@gmail.com>
2021-02-01 04:39:31 -0500

    required routes from travel-controller and invoked them

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 // Require routes
-
+const travelRoutes = require("./controller/travel-controller");
 
 // Sets up the Express App
 const app = express();
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Invoke routes
-
+app.use(travelRoutes);
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync({ force: true }).then(() => {
