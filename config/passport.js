f48322adae165f7f401c7f3af78e407bf4b57c06
Joe Maniaci <joe.maniaci1@gmail.com>
2021-02-03 19:28:13 -0500

    updated passport.js

--- config/passport.js
+++ config/passport.js
@@ -3,3 +3,36 @@ const LocalStrategy = require('passport-local').Strategy;
 
 const db = require("../models");
 
+passport.use(new LocalStrategy(
+  {
+    usernameField: "email"
+  },
+  function(email, password, done) {
+    db.User.findOne({
+      where: {
+        email: email,
+      }
+    },
+    function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { meesage: 'Incorrect email.' });
+      }
+      if (!user.validePassword(password)) {
+        return done(null, false, { message: 'Incorrect password'});
+      }
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Exporting our configured passport
+module.exports = passport;
