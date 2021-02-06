235b65eacc2e5489a1fa7aa1fff6e9a3a8ae7b8f
Joe Maniaci <joe.maniaci1@gmail.com>
2021-02-06 12:03:31 -0500

    updated review controller.js

--- controller/review-controller.js
+++ controller/review-controller.js
@@ -9,13 +9,13 @@ const client = new Client({ node: 'https://yahuaxydlj:p1p8dt5y8g@birch-114820214
 
 //Retrieves all posts
 //Renders it to index html file
-router.get('/api/reviews', async (req, res) => {
+router.get('reviews', async (req, res) => {
   
   var result = await db.Travel.findAll({
     include: [db.User],
   });
-  //res.render('index',result);
-  res.json(result);
+  res.render('allreviews',result);
+  
 });
 
 //Retrieves all posts of a specific city
