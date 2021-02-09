16f634dbb6ec1bf6c8ef73e2cf14679042fccc03
Joe Maniaci <joe.maniaci1@gmail.com>
2021-02-09 13:45:02 -0500

    added the /search route to html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -35,6 +35,11 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/write_review.html"));
   });
 
+  app.get("/search", isAuthenticated, function(req, res) {
+    
+    res.sendFile(path.join(__dirname, "../public/search"));
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/main", isAuthenticated, function(req, res) {
